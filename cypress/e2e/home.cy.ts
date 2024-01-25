1b81b7b500abb81585de323a9747860546ff446b
Joselyn Obando <obandofernandez1@gmail.com>
2024-01-25 16:28:38 +0100

    Creando pasos iniciales para Cypress

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it("the h1 contains the correct text", () => {
+    cy.visit('http://localhost:3000')
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+
+    it.only("the features on the homepage are correct", () => {
+      cy.visit('http://localhost:3000')})
+    })
